380a4f5bd97c6f2030caf0a85d40150a5ddd671a
mdyusuf2105-design <mdyusuf2105@gmail.com>
2026-08-03 11:17:08 +0530

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
+from fastapi.responses import JSONResponse
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="FastAPI Backend",
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
@@ -18,3 +36,22 @@ def version():
     return {
         "version": "1.0.0"
     }
+
+@app.on_event("startup")
+async def startup():
+    logger.info("FastAPI application started.")
+
+@app.on_event("shutdown")
+async def shutdown():
+    logger.info("FastAPI application stopped.")
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Unhandled Exception: {exc}")
+
+    return JSONResponse(
+        status_code=500,
+        content={
+            "message": "Internal Server Error"
+        }
+    )
